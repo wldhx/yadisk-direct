c39e0fc5e343eb75c32e823560e38e4b58008a20
Dmitriy Volkov <wldhx+vcs+gitlab_com@wldhx.me>
2016-06-01 02:47:55 +0300

    Move link fetching logic into a separate function

--- wldhx/yadisk_direct/main.py
+++ wldhx/yadisk_direct/main.py
@@ -4,12 +4,16 @@ import requests
 API_ENDPOINT = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
 
 
+def get_real_direct_link(sharing_link: str) -> str:
+    pk_request = requests.get(API_ENDPOINT.format(sharing_link))
+
+    return pk_request.json()['href']
+
+
 def main():
     yad_url = argv[1]
 
-    pk_request = requests.get(API_ENDPOINT.format(yad_url))
-
-    print(pk_request.json()['href'])
+    print(get_real_direct_link(yad_url))
 
 
 if __name__ == '__main__':

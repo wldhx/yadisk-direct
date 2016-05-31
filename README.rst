9a91a8c36867b179b7430c6461bdda47b5673a70
Dmitriy Volkov <wldhx+vcs+gitlab_com@wldhx.me>
2016-05-31 15:50:36 +0300

    Import initial package, README, LICENSE and .gitignore

---
+++ README.rst
@@ -0,0 +1,13 @@
+Yandex.Disk direct links extractor
+==================================
+
+Get real direct links usable with tools like ``curl`` or ``wget`` for
+files stored in `Yandex.Disk <https://disk.yandex.ru>`__.
+
+Disclaimer
+----------
+
+While this code depends on an open Yandex's
+`API <https://tech.yandex.com/disk/api/reference/public-docpage/#download>`__,
+I heartily recommend you to not use it in anything resembling production
+environments.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+setup(
+    name='yadisk_direct',
+    version='0.0.1',
+    description="Get real direct links usable with tools like curl or wget for files stored in Yandex.Disk",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    keywords='Yandex.Disk',
+    author='wldhx',
+    author_email='wldhx+pypi_python_org@wldhx.me',
+    url='https://gitlab.com/wldhx/yadisk_direct',
+    license='GPLv3',
+    packages=find_packages(exclude=['examples', 'tests']),
+    zip_safe=True,
+    install_requires=[
+        'requests',
+    ],
+    entry_points="""
+      # -*- Entry points: -*-
+      """, )

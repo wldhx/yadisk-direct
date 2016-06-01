1b561313544eafe7b58a5b5d3b7d3494510cde9c
Dmitriy Volkov <wldhx+vcs+gitlab_com@wldhx.me>
2016-06-01 03:26:47 +0300

    Refactor

--- wldhx/yadisk_direct/main.py
+++ wldhx/yadisk_direct/main.py
@@ -12,13 +12,13 @@ def get_real_direct_link(sharing_link: str) -> str:
 
 def main():
     parser = argparse.ArgumentParser(description='Get real direct links usable with tools like curl or wget for files stored in Yandex.Disk.')
-    parser.add_argument('sharing_links', type=str, nargs='+',
-            help='YaDisk sharing links (like https://yadi.sk/i/LKkWupFjr5WzR)')
+    parser.add_argument('sharing_link', type=str, nargs='+',
+            help='YaDisk sharing link (like https://yadi.sk/i/LKkWupFjr5WzR)')
     parser.add_argument('-s', '--separator', type=str, default=' ',
             help='A string to separate output links with')
     args = parser.parse_args()
 
-    print(*[get_real_direct_link(x) for x in args.sharing_links], sep=args.separator)
+    print(*[get_real_direct_link(x) for x in args.sharing_link], sep=args.separator)
 
 
 if __name__ == '__main__':
